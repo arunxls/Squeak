c3e819b015372dd152914d2536ba435f892fb89d
Arun Krishnakumar <arunxls@gmail.com>
2015-10-10 02:01:18 -0500

    Added client

---
+++ Squeak/SqueakClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Squeak
+{
+    class SqueakClient
+    {
+        public static void test()
+        {
+            // This constructor arbitrarily assigns the local port number.
+            int port = 999;
+            String ip = "www.contoso.com";
+            UdpClient udpClient = new UdpClient(port);
+
+            udpClient.Connect(ip, port);
+
+            // Sends a message to the host to which you have connected.
+            Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+
+            udpClient.Send(sendBytes, sendBytes.Length);
+            udpClient.Close();
+        }
+    }
+}
