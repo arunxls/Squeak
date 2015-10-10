bf7300a9eeb7ea892be9c352875448683739ac2f
psakuru <sp.prithvi@gmail.com>
2015-10-10 01:07:23 -0500

    server skeleton

---
+++ Squeak/SqueakServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Squeak
+{
+    class SqueakServer
+    {
+        private TcpListener serverSocket;
+        private TcpClient clientSocket;
+
+        public SqueakServer() {
+
+        }
+
+        public void initializeServer(int portNumber) {
+            this.serverSocket = null;
+            try
+            {
+                this.serverSocket = new TcpListener(9999);
+
+            }
+            catch (SocketException se) {
+            }
+        } 
+    }
+}
